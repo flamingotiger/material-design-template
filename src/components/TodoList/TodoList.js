e5e0a4625b6969cc4c635d1cc29025b8d9efda4c
hohabu <hhbhong@gmail.com>
2018-03-01 01:08:34 +0900

    TodoList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import './TodoList.css';
 
+const TodoList = ({form, children}) => {
+  return (
+    <main className="todo-list-template">
+      <div className='title'>
+      오늘 할 일
+      </div>
+      <section className="form-wrapper">
+        { form }
+      </section>
+      <section className="totos-wrapper">
+        { children }
+      </section>
+    </main>
+  )
+}
+
+export default TodoList;
